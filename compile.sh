22e839f0ccb6a97c3c95b71edce21e7a9152206b
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-05-01 10:57:17 +0800

    Create shell script for compiling c++ program

---
+++ compile.sh
@@ -0,0 +1,16 @@
+# Compile script for C++ program
+
+# compiler flags, includes and libs
+OPENCV_PKG_CONFIG=`pkg-config --cflags --libs opencv`
+BOOST_FLAGS="-I/usr/local/opt/boost/include -lboost_system -lboost_filesystem"
+
+# remove the compiled directory
+rm -rf ./out/compiled
+
+# create the folders
+mkdir -p out/compiled
+
+# compile the program
+g++ $OPENCV_PKG_CONFIG $BOOST_FLAGS -std=c++11 src/color-map.cpp -o out/compiled/color-map
+
+
