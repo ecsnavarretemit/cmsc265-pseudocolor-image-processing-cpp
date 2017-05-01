df8ec7129b350723b8583de39d85a674ee0ed4b8
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-05-01 12:31:56 +0800

    Output status messages when running the compile shell script

--- compile.sh
+++ compile.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env bash
+
+# compile.sh
+#
+# Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+# Licensed under MIT
+# Version 1.0.0-alpha
+
 # Compile script for C++ program
 
 # compiler flags, includes and libs
-OPENCV_PKG_CONFIG=`pkg-config --cflags --libs opencv`
+OPENCV_FLAGS=`pkg-config --cflags --libs opencv`
 BOOST_FLAGS="-I/usr/local/opt/boost/include -lboost_system -lboost_filesystem"
+
+echo "Removing previously compiled programs."
 
 # remove the compiled directory
 rm -rf ./out/compiled
@@ -10,8 +20,12 @@ rm -rf ./out/compiled
 # create the folders
 mkdir -p out/compiled
 
+echo "Compiling source codes."
+
 # compile the program
-g++ $OPENCV_PKG_CONFIG $BOOST_FLAGS -std=c++11 src/color-map.cpp -o out/compiled/color-map
-g++ $OPENCV_PKG_CONFIG $BOOST_FLAGS -std=c++11 src/rgb-transformation.cpp -o out/compiled/rgb-transformation
+g++ $OPENCV_FLAGS $BOOST_FLAGS -std=c++11 src/color-map.cpp -o out/compiled/color-map
+g++ $OPENCV_FLAGS $BOOST_FLAGS -std=c++11 src/rgb-transformation.cpp -o out/compiled/rgb-transformation
+
+echo "Compilation done."
 
 
