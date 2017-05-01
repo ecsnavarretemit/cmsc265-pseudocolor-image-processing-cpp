10589e87b7a0c28e7f9da7d9ed44bdd5954c768e
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-05-02 07:34:33 +0800

    Extract finding images functionality into a function

---
+++ src/files.cpp
@@ -0,0 +1,52 @@
+/**
+ * files.cpp
+ *
+ * Copyright(c) Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
+ * Licensed under MIT
+ * Version 1.0.1
+ */
+
+#include <vector>
+#include "boost/filesystem.hpp"
+#include "boost/range/iterator_range.hpp"
+
+using namespace std;
+
+vector<boost::filesystem::path> get_images(string directory, vector<string> extensions = vector<string>()) {
+    boost::filesystem::path boost_directory(directory);
+    vector<string> extensions_filter;
+    vector<boost::filesystem::path> images;
+
+    // throw error
+    if (!boost::filesystem::is_directory(directory)) {
+        throw invalid_argument("Directory: " + directory + " is not a directory.");
+    }
+
+    // use the extensions parameter if the number of items is greater than 0
+    // else use jpg and png as defaults
+    if (extensions.size() > 0) {
+        extensions_filter = extensions;
+    } else {
+        extensions_filter.push_back(".jpg");
+        extensions_filter.push_back(".png");
+    }
+
+    // add the matched paths to the vector and return it later if it contains more than 0 elements.
+    for(auto& entry : boost::make_iterator_range(boost::filesystem::directory_iterator(boost_directory), {})) {
+        for (auto & extension : extensions_filter) {
+            if (!boost::filesystem::is_directory(entry) && entry.path().extension() == extension) {
+                images.push_back(entry);
+                break;
+            }
+        }
+    }
+
+    // throw error message saying no images found
+    if (images.size() == 0) {
+        throw "No images found in the directory: " + directory;
+    }
+
+    return images;
+}
+
+
--- src/rgb-transformation.cpp
+++ src/rgb-transformation.cpp
@@ -13,6 +13,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "boost/filesystem.hpp"
 #include "boost/range/iterator_range.hpp"
+#include "files.cpp"
 
 using namespace std;
 
@@ -55,63 +56,67 @@ int main() {
     // variable to use in storing image dimensions
     cv::Size im_size;
 
-    // read all images in the folder
-    for(auto& entry : boost::make_iterator_range(boost::filesystem::directory_iterator(in), {})) {
-        // skip iteration when the file does not have ".jpg" file extension
-        if (entry.path().extension() != ".jpg") {
-            continue;
+    try {
+        vector<boost::filesystem::path> imgs = get_images(in_path);
+
+        for (auto & img : imgs) {
+             // assemble the output path
+            boost::filesystem::path im_out = out / img.filename();
+
+            // read image in grayscale
+            src = cv::imread(img.string(), CV_LOAD_IMAGE_GRAYSCALE);
+
+            // create Size object
+            im_size = cv::Size(src.cols, src.rows);
+
+            // create empty mat for placing the processed values for each channel
+            red = cv::Mat::zeros(im_size, CV_8UC1);
+            green = cv::Mat::zeros(im_size, CV_8UC1);
+            blue = cv::Mat::zeros(im_size, CV_8UC1);
+
+            // loop through each pixel using Mat::forEach and C++11 lambda.
+            src.forEach<Pixel>([&a, &b, &c, &red, &green, &blue](Pixel &pixel, const int * position) -> void {
+                // compute the value of bx
+                float bx = b * (int) pixel;
+
+                // perform transformation on the r channel: R = a | sin(bx) |
+                red.at<uchar>(position[0], position[1]) = a * abs(sin(bx));
+
+                // perform transformation on the g channel: G = a | sin(bx + c) |
+                green.at<uchar>(position[0], position[1]) = a * abs(sin(bx + c));
+
+                // perform transformation on the b channel: B = a | sin(bx + 2c) |
+                blue.at<uchar>(position[0], position[1]) = a * abs(sin(bx + (2 * c)));
+            });
+
+            // add the channels in to the vector matrix in the arrangement of bgr
+            // since opencv reads rgb images in bgr not rgb
+            rgb_vector.push_back(blue);
+            rgb_vector.push_back(green);
+            rgb_vector.push_back(red);
+
+            // merge the channels
+            cv::merge(rgb_vector, dst);
+
+            // write to the filesystem
+            cv::imwrite(im_out.string(), dst);
+
+            // empty out the vector for the next iteration
+            rgb_vector.clear();
+
+            // free up resources
+            src.release();
+            dst.release();
+            red.release();
+            green.release();
+            blue.release();
         }
-
-        // assemble the output path
-        boost::filesystem::path im_out = out / entry.path().filename();
-
-        // read image in grayscale
-        src = cv::imread(entry.path().string(), CV_LOAD_IMAGE_GRAYSCALE);
-
-        // create Size object
-        im_size = cv::Size(src.cols, src.rows);
-
-        // create empty mat for placing the processed values for each channel
-        red = cv::Mat::zeros(im_size, CV_8UC1);
-        green = cv::Mat::zeros(im_size, CV_8UC1);
-        blue = cv::Mat::zeros(im_size, CV_8UC1);
-
-        // loop through each pixel using Mat::forEach and C++11 lambda.
-        src.forEach<Pixel>([&a, &b, &c, &red, &green, &blue](Pixel &pixel, const int * position) -> void {
-            // compute the value of bx
-            float bx = b * (int) pixel;
-
-            // perform transformation on the r channel: R = a | sin(bx) |
-            red.at<uchar>(position[0], position[1]) = a * abs(sin(bx));
-
-            // perform transformation on the g channel: G = a | sin(bx + c) |
-            green.at<uchar>(position[0], position[1]) = a * abs(sin(bx + c));
-
-            // perform transformation on the b channel: B = a | sin(bx + 2c) |
-            blue.at<uchar>(position[0], position[1]) = a * abs(sin(bx + (2 * c)));
-        });
-
-        // add the channels in to the vector matrix in the arrangement of bgr
-        // since opencv reads rgb images in bgr not rgb
-        rgb_vector.push_back(blue);
-        rgb_vector.push_back(green);
-        rgb_vector.push_back(red);
-
-        // merge the channels
-        cv::merge(rgb_vector, dst);
-
-        // write to the filesystem
-        cv::imwrite(im_out.string(), dst);
-
-        // empty out the vector for the next iteration
-        rgb_vector.clear();
-
-        // free up resources
-        src.release();
-        dst.release();
-        red.release();
-        green.release();
-        blue.release();
+    } catch(const invalid_argument& e) {
+        cerr << e.what() << endl;
+        return 1;
+    } catch (const string msg) {
+        cerr << msg << endl;
+        return 1;
     }
 
     cout << "Done processing images." << endl;
